9c21cea1da0973978fc597c39cfe0ebcff3c5b8d
Ali Haberfield <hello@alihaberfield.com>
2014-09-22 10:49:42 +0200

    [task] Add icon font

---
+++ app/assets/js/modules/modal.jquery.js
@@ -0,0 +1,11 @@
+var modalWindow = {
+
+    init : function() {
+        console.log("modalWindow init update")
+        console.log($('.reveal-modal').html())
+        $('.reveal-modal').show();
+    }
+
+}
+
+
